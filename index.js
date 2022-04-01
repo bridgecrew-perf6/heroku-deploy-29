1d21574c3672d36408af84e34bb114f1065cf542
murali0101 <muralimv0101@gmail.com>
2022-04-01 09:58:50 +0530

    second commit

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require("express")
 const app = express()
 app.use(express.json())
+app.get("/", (req, res) => { 
+    res.send("hello welcome")
+})
+app.get("/greet", (req, res) => { 
+    res.send("hello welcome guys")
+})
 var port=process.env.PORT || 5000
 app.listen(port, () => {
     console.log(`listening port ${port}`)
