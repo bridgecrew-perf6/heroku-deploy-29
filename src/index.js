145f13d58a918cd604646ded7e1db17affb09732
murali0101 <muralimv0101@gmail.com>
2022-04-01 10:17:59 +0530

    r

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require("express")
 const app = express()
 app.use(express.json())
 app.get("/", (req, res) => { 
-    res.send("hello welcome server src in")
+    res.send(`hello welcome server src in ${process.env.NAME}`)
 })
 app.get("/greet", (req, res) => { 
     res.send("good morning guys server")
