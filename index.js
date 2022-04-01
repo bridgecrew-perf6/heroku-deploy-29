656cd89b9627992d52f1fc7ea6a92cfea9010f25
murali0101 <muralimv0101@gmail.com>
2022-04-01 10:05:10 +0530

    third commit

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.get("/", (req, res) => {
     res.send("hello welcome")
 })
 app.get("/greet", (req, res) => { 
-    res.send("hello welcome guys")
+    res.send("good morning guys")
 })
 var port=process.env.PORT || 5000
 app.listen(port, () => {
