2e3ff52e4de520bd01e481696cf31a51d437c3c8
murali0101 <muralimv0101@gmail.com>
2022-04-01 10:08:08 +0530

    fourth commit

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require("express")
 const app = express()
 app.use(express.json())
 app.get("/", (req, res) => { 
-    res.send("hello welcome")
+    res.send("hello welcome server")
 })
 app.get("/greet", (req, res) => { 
-    res.send("good morning guys")
+    res.send("good morning guys server")
 })
 var port=process.env.PORT || 5000
 app.listen(port, () => {
